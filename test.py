d0a8f9f0c727e28be4da57052b386fd6f3065a60
BitLittle <opera.operaciy@yandex.ru>
2022-09-20 01:48:51 +0700

    test

--- test.py
+++ test.py
@@ -11,24 +11,30 @@ def test():
 
         print('kek1')
         start = time.time()
+
         rand_user = db.query(
             Users.id.label('user_id'),
             Users.FIO,
             Users.age,
             Research.id,
-            Research.main_photo_path
+            Research.main_photo_path,
+            func.count(Research.id).label('count_research')
         ).join(
             Research,
             Research.user_id == Users.id
-        ).filter(
-            and_(Research.checked == True)
-        ).group_by(Users.id).all()
+        ).filter(Research.checked == True).group_by(Users.id).limit(4).all()
+
         users = []
         for i in rand_user:
-            count_research = db.query(Research).filter(and_(Research.user_id == i.user_id, Research.checked == True)).count()
             count_votes = db.query(Votes).join(Research, Research.id == Votes.user_vote_to_research).filter(and_(Votes.user_research == i.user_id, Research.checked == True)).count()
-            users.append([i, count_research, count_votes])
-        users = sorted(users, key=lambda x: x[2], reverse=True)[0:4] if users != [] else None
+            users.append([i, count_votes])
+        users = sorted(users, key=lambda x: x[1], reverse=True) if users != [] else None
+        # users = []
+        # for i in rand_user:
+        #     count_research = db.query(Research).filter(and_(Research.user_id == i.user_id, Research.checked == True)).count()
+        #     count_votes = db.query(Votes).join(Research, Research.id == Votes.user_vote_to_research).filter(and_(Votes.user_research == i.user_id, Research.checked == True)).count()
+        #     users.append([i, count_research, count_votes])
+        # users = sorted(users, key=lambda x: x[2], reverse=True)[0:4] if users != [] else None
         end = time.time()
         print(f'delta: {end-start}')
         print(users)
